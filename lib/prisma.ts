bc69ad42cbcdafdecc5153dcbfbbfdfea3059a1d
nfl-alvis <yoikiopang@gmail.com>
2026-08-01 19:11:01 +0700

    fix: remove deprecated Prisma client datasource option

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,3 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+export const prisma = new PrismaClient();
